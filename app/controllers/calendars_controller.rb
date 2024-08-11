f494f11b0870dc49633d510b4f25a2a67d0c97cd
yuuukikiki <yuutan0444techcamp@gmail.com>
2024-08-11 20:52:51 +0900

    Issue2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
+    plan.create(plan_params)
     redirect_to action: :index
   end
 
@@ -27,7 +27,7 @@ class CalendarsController < ApplicationController
 
     @week_days = []
 
-    plans = Plan.where(date: @todays_date..@todays_date + 6)
+    plans = plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
       today_plans = []
